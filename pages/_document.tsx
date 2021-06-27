47eba69c89fb0eacea6b09d8ac25a8cbdf0160ed
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2021-06-27 15:00:58 +0200

    set up scaffold

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="Hi, I'm Markus Hsi-Yang Fritz, computational biologist turned software engineer. I ♥ web technology, open source, food & hiking."
+          />
+          <link rel="icon" type="image/png" href="/favicon.png"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <h1 className="text-xl text-blue-500">Hello, World!</h1>;
+}
